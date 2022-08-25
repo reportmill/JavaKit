63d719158885345e91f62be2ea36b450c57c4f01
Jeff Martin <jeff@reportmill.com>
2022-08-25 16:52:58 -0500

    Some improvements

--- src/javakit/shell/JavaTextDoc.java
+++ src/javakit/shell/JavaTextDoc.java
@@ -1,12 +1,14 @@
 package javakit.shell;
-import javakit.parse.JClassDecl;
-import javakit.parse.JInitializerDecl;
-import javakit.parse.JFile;
-import javakit.parse.JavaParser;
+import javakit.parse.*;
+import javakit.text.JavaTextArea;
 import snap.props.PropChange;
+import snap.text.TextBox;
 import snap.text.TextDoc;
 import snap.text.TextDocUtils;
+import snap.text.TextLine;
 import snap.util.ArrayUtils;
+
+import java.util.stream.Stream;
 
 /**
  * This class holds the text of a Java file with methods to easily build.
@@ -53,20 +55,12 @@ public class JavaTextDoc extends TextDoc {
         super.setString(aString);
         _jfile = null;
 
-        // Get JFile
-        JFile jfile = getJFile();
+        // Get initializer decls statement blocks
+        JStmtBlock[] initDeclsBlocks = getInitDeclBlocks();
 
-        // Get static decls
-        JClassDecl classDecl = jfile.getClassDecl();
-        JInitializerDecl[] initDecls = classDecl.getInitDecls();
-
-        // Create blocks
-        for (JInitializerDecl initDecl : initDecls) {
-
-            // Create block
-            Block block = new Block();
-            block._start = initDecl.getStart();
-            block._end = initDecl.getStart();
+        // Iterate over init decls statement blocks and create block
+        for (JStmtBlock blockStmt : initDeclsBlocks) {
+            Block block = new Block(blockStmt);
             addBlock(block);
         }
     }
@@ -113,6 +107,74 @@ public class JavaTextDoc extends TextDoc {
     }
 
     /**
+     * Returns the last block.
+     */
+    public Block getBlockLast()
+    {
+        int blockCount = getBlockCount();
+        return blockCount > 0 ? getBlock(blockCount - 1) : null;
+    }
+
+    /**
+     * Returns the empty block at end.
+     */
+    public Block getEmptyBlock()
+    {
+        // Iterate over blocks and return empty one
+        Block lastBlock = getBlockLast();
+        if (lastBlock != null && lastBlock.getString().length() == 0)
+            return lastBlock;
+
+        // Otherwise add empty and return
+        Block emptyBlock = addEmptyBock();
+        return emptyBlock;
+    }
+
+    /**
+     * Adds an empty block at end.
+     */
+    public Block addEmptyBock()
+    {
+        // Get char index of closing '}' in Class decl (really get line start for closing char)
+        JFile jFile = getJFile();
+        JClassDecl classDecl = jFile.getClassDecl();
+        int endCharIndex = classDecl.getEnd();
+        TextLine endLine = getLineForCharIndex(endCharIndex);
+        endCharIndex = endLine.getStart();
+
+        // Add block chars
+        addChars("{\n}\n\n", null, endCharIndex);
+
+        // Get new last init decl statement block
+        JStmtBlock[] initBlocks = getInitDeclBlocks();
+        JStmtBlock lastBlock = initBlocks[initBlocks.length - 1];
+
+        // Create/add new block
+        Block newBlock = new Block(lastBlock);
+        addBlock(newBlock);
+        return newBlock;
+    }
+
+    /**
+     * Returns the JInitializerDecls.
+     */
+    private JStmtBlock[] getInitDeclBlocks()
+    {
+        // Get initializer decls
+        JFile jFile = getJFile();
+        JClassDecl classDecl = jFile.getClassDecl();
+        JInitializerDecl[] initDecls = classDecl.getInitDecls();
+
+        // Convert initializer decls array to block statement array
+        Stream<JInitializerDecl> initDeclsStream = Stream.of(initDecls);
+        Stream<JStmtBlock> stmtBlockStream = initDeclsStream.map(id -> id.getBlock());
+        JStmtBlock[] stmtBlocks = stmtBlockStream.toArray(size -> new JStmtBlock[size]);
+
+        // Return
+        return stmtBlocks;
+    }
+
+    /**
      * Override to detect char changes.
      */
     @Override
@@ -124,40 +186,31 @@ public class JavaTextDoc extends TextDoc {
         // Handle CharsChange
         String propName = aPC.getPropName();
         if (propName == Chars_Prop)
-            textDidChange((TextDocUtils.CharsChange) aPC);
+            textDidCharsChange((TextDocUtils.CharsChange) aPC);
     }
 
     /**
      * Called when text changes.
      */
-    private void textDidChange(TextDocUtils.CharsChange aCharsChange)
+    private void textDidCharsChange(TextDocUtils.CharsChange aCharsChange)
     {
         // Clear JFile
         _jfile = null;
 
         // Call didAddChars/didRemoveChars
         int charIndex = aCharsChange.getIndex();
-        CharSequence addChars = aCharsChange.getNewValue();
-        CharSequence removeChars = aCharsChange.getOldValue();
-        if (addChars != null)
-            textDidAddChars(addChars, charIndex);
-        else textDidRemoveChars(removeChars, charIndex);
-    }
+        //CharSequence addChars = aCharsChange.getNewValue();
+        //CharSequence removeChars = aCharsChange.getOldValue();
+        //if (addChars != null) textDidAddChars(addChars, charIndex);
+        //else textDidRemoveChars(removeChars, charIndex);
 
-    /**
-     * Called when characters are added to text.
-     */
-    protected void textDidAddChars(CharSequence theChars, int charIndex)
-    {
-
-    }
-
-    /**
-     * Called when characters are removed from text.
-     */
-    protected void textDidRemoveChars(CharSequence theChars, int charIndex)
-    {
-
+        // Reset blocks
+        Block[] blocks = getBlocks();
+        for (Block block : blocks) {
+            if (charIndex < block.getStartCharIndex() && block._textArea != null)
+                block.resetTextBoxRange();
+            else break;
+        }
     }
 
     /**
@@ -165,17 +218,73 @@ public class JavaTextDoc extends TextDoc {
      */
     public class Block {
 
-        // The start/end char index
-        private int  _start, _end;
+        // The start/end line of block
+        private TextLine  _startLine, _endLine;
 
-        // The start/end line index
-        private int  _startLineIndex, _endLineIndex;
+        // The JavaTextArea
+        private JavaTextArea  _textArea;
 
         /**
          * Constructor.
          */
-        public Block()  { }
+        public Block(JStmtBlock blockStmt)
+        {
+            int startCharIndex = blockStmt.getStart();
+            int endCharIndex = blockStmt.getEnd();
+            _startLine = getLineForCharIndex(startCharIndex);
+            _endLine = getLineForCharIndex(endCharIndex);
+        }
 
+        /**
+         * Returns the start char index.
+         */
+        public int getStartCharIndex()  { return _startLine.getEnd(); }
 
+        /**
+         * Returns the end char index.
+         */
+        public int getEndCharIndex()  { return _endLine.getStart(); }
+
+        /**
+         * Returns a JavaTextArea.
+         */
+        public JavaTextArea getTextArea()
+        {
+            // If already set, just return
+            if (_textArea != null) return _textArea;
+
+            // Create/config
+            JavaTextArea textArea = new JavaTextArea();
+            textArea.setTextDoc(JavaTextDoc.this);
+            resetTextBoxRange();
+
+            // Set/return
+            return _textArea = textArea;
+        }
+
+        /**
+         * Resets the TextBox start/end char index.
+         */
+        public void resetTextBoxRange()
+        {
+            // If no TextArea, just return
+            if (_textArea == null) return;
+
+            // Set TextBox char range
+            TextBox textBox = _textArea.getTextBox();
+            textBox.setStart(getStartCharIndex());
+            textBox.setEndCharIndex(getEndCharIndex());
+        }
+
+        /**
+         * Returns the string for block.
+         */
+        public String getString()
+        {
+            int startCharIndex = getStartCharIndex();
+            int endCharIndex = getEndCharIndex();
+            String str = JavaTextDoc.this.subSequence(startCharIndex, endCharIndex).toString().trim();
+            return str;
+        }
     }
 }
